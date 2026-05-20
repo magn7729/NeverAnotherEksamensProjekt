e2033cc988efe1572c3e4e639827b8557eb5dce5
oliver <oliverbf@icloud.com>
2026-05-20 14:03:24 +0200

    Tilføjet forklaringer til Screen.kt

--- app/src/main/java/com/example/neveranothereksamensprojekt/navigation/Screen.kt
+++ app/src/main/java/com/example/neveranothereksamensprojekt/navigation/Screen.kt
@@ -1,22 +1,23 @@
 package com.example.neveranothereksamensprojekt.navigation
 
-sealed class Screen (val route: String){ // Hvad betyder sealed?
+//Oliver
 
-    // Første side
+// Screen.kt bruges til at samle alle appens navigationsruter ét sted
+// En route er navnet på den side, som NavHost kan navigere til
+// Det gør vi for at undgå, at skrive "home", "intro" osv. manuelt rundt omkring i appen
+
+sealed class Screen (val route: String){ // Screen er en sealed class, som vi bruger til at definere alle de skærme, appen kan navigere til
+                                         // Screen er en sealed class med en primary constructor, der kræver en route af datatypen String
+
+    // Vi opretter Home som et data object, der arver fra Screen.
+    // Vi sender "home" ind som route, så Screen.Home.route returnerer "home".
+    // I AppNavHost matcher vi denne route med Homescreen.
+    // Det betyder, at når navigationen går til "home", vises Homescreen.
     data object Home : Screen("home")
-
-    // Intro-side
-    data object Intro : Screen("intro")
-
-    // Indtast dine mål side
+    data object Intro : Screen("intro")                         // Samme princip for alle data objekter
     data object Measurements : Screen("measurements")
-
-    // Resultat side
     data object Result : Screen("result")
-
-    // Kurv
     data object Checkout : Screen("checkout")
-
-    // Bekræftelse af ordre side
     data object Confirmation : Screen("confirmation")
 }
+
