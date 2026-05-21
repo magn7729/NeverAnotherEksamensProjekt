024a5be5c2cf14157bf1a121c097f0a62a859d6e
gume0002 <gmeyer171@gmail.com>
2026-05-21 13:08:02 +0200

    Oprettet ViewModel

--- app/src/main/java/com/example/neveranothereksamensprojekt/viewmodel/BraViewModel.kt
+++ app/src/main/java/com/example/neveranothereksamensprojekt/viewmodel/BraViewModel.kt
@@ -1,3 +1,14 @@
 package com.example.neveranothereksamensprojekt.viewmodel
 
+import androidx.lifecycle.ViewModel
+
 //Viewmodel test
+
+// ViewModel holder på appens state og logik.
+
+// Den skal være en almindelig class, ikke en data class.
+
+class BraViewModel : ViewModel() {
+
+
+}
