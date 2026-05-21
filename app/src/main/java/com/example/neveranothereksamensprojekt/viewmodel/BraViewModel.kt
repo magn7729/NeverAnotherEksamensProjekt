1da1b090b6190502463c1266ea0972f0ab724e76
gume0002 <gmeyer171@gmail.com>
2026-05-21 14:20:56 +0200

    Lavet 4 sliders og state hoistet til side 3

--- app/src/main/java/com/example/neveranothereksamensprojekt/viewmodel/BraViewModel.kt
+++ app/src/main/java/com/example/neveranothereksamensprojekt/viewmodel/BraViewModel.kt
@@ -1,5 +1,9 @@
 package com.example.neveranothereksamensprojekt.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 //Viewmodel test
@@ -7,10 +11,32 @@ import androidx.lifecycle.ViewModel
 // ViewModel holder på appens state og logik.
 
 class BraViewModel : ViewModel() {
+    var upperCircumference by mutableFloatStateOf(100f)
 
-    // State skal være her
+    var lowerCircumference by mutableFloatStateOf(100f)
 
-    // State på slider
-    // State på side 5 - accept terms
+    var breastHeight by mutableFloatStateOf(100f)
+
+    var breastWidth by mutableFloatStateOf(100f)
+
+
+
+    // Funktioner der opdaterer hver enkelt slider-state
+
+    fun updateUpperCircumference(value: Float) {
+        upperCircumference = value
+    }
+
+    fun updateLowerCircumference(value: Float) {
+        lowerCircumference = value
+    }
+
+    fun updateBreastHeight(value: Float) {
+        breastHeight = value
+    }
+
+    fun updateBreastWidth(value: Float) {
+        breastWidth = value
+    }
 
 }
